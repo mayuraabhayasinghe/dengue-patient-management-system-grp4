f215b16fcbcaf30fc79d88958f34d9ff5fe6d517
Thushan2002 <selvakumarthushanthan4@gmail.com>
2025-04-19 20:54:20 +0530

    feat: Hero section added

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,13 @@
 import React from "react";
 import { Route, Routes } from "react-router-dom";
 import Navbar from "./Components/navbar";
+import HomePage from "./Pages/HomePage";
 
 const App = () => {
   return (
     <div>
       <Navbar />
+      <HomePage />
     </div>
   );
 };
