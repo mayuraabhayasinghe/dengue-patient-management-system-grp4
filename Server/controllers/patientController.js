df7174d3ee44f9ded15cce2491155f340f35fe52
mayuraabhayasinghe <abhayasinghemayura@gmail.com>
2025-04-25 23:04:10 +0530

    started developing patient controller

---
+++ Server/controllers/patientController.js
@@ -0,0 +1,6 @@
+const User = require("../models/userModel");
+const Patient = require("../models/patientModel");
+const nodemailer = require("nodemailer");
+const bcrypt = require("bcrypt");
+
+const addPatient = async () => {};
