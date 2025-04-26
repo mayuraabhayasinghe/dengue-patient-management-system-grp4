55b00e2e30f2cb3926058481d2411b63a5bece0a
Nadun Thilina <152951157+nadunthilina@users.noreply.github.com>
2025-04-26 18:40:58 +0530

    Add password & Forget Password

--- Client/src/Pages/Login.jsx
+++ Client/src/Pages/Login.jsx
@@ -31,6 +31,7 @@ const Login = () => {
 
           {/* Main Content */}
       <div className="container mx-auto px-4 mt-12 flex justify-between items-center">
+
         {/* Login Form */}
         <div className="w-1/2">
           <h1 className="text-4xl font-bold mb-4">Log In to Your Account</h1>
@@ -49,3 +50,34 @@ const Login = () => {
               />
             </div>
             
+            <div>
+              <label htmlFor="password" className="block text-gray-700 mb-2">Password</label>
+              <input
+                type="password"
+                id="password"
+                value={password}
+                onChange={(e) => setPassword(e.target.value)}
+                className="w-full px-4 py-3 rounded-lg bg-gray-50 border border-gray-200 focus:outline-none focus:border-primary"
+                placeholder="password"
+              />
+            </div>
+
+            <div className="text-right">
+              <a href="/forgot-password" className="text-gray-600 hover:text-primary">
+                Forgot Password?
+              </a>
+            </div>
+
+            <button
+              type="submit"
+              className="w-full bg-primary text-white py-3 rounded-lg hover:bg-primary-dark transition-colors"
+            >
+              Log In
+            </button>
+
+            <p className="text-center text-gray-600">
+              Don't have an account?{' '}
+              <a href="/signup" className="text-primary hover:underline">Sign Up</a>
+            </p>
+          </form>
+        </div>
