b88758817cac81a74acc6e2f721c83d0195f60e2
Nadun Thilina <152951157+nadunthilina@users.noreply.github.com>
2025-04-26 18:28:34 +0530

    Handle login logic

--- Client/src/Pages/Login.jsx
+++ Client/src/Pages/Login.jsx
@@ -2,3 +2,13 @@ import React, { useState } from 'react';
 import doctorImage from '../assets/doctorlogin.png';
 import logo from '../assets/logo.png';
 import logowhite from '../assets/logo_white.png';
+
+const Login = () => {
+    const [username, setUsername] = useState('');
+    const [password, setPassword] = useState('');
+  
+    const handleSubmit = (e) => {
+      e.preventDefault();
+      // Handle login logic here
+    };
+  
