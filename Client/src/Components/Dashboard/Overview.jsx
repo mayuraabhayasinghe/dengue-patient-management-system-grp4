f300dfbcf83cebf3527b8d359387a53ba3021696
Thushan2002 <selvakumarthushanthan4@gmail.com>
2025-05-07 19:43:59 +0530

    feat: dashboard overview initialized

--- Client/src/Components/Dashboard/Overview.jsx
+++ Client/src/Components/Dashboard/Overview.jsx
@@ -1,7 +1,16 @@
 import React from "react";
 
 const Overview = () => {
-  return <div>Overview</div>;
+  return (
+    <div className="p-2 md:p-4">
+      <div className="grid grid-cols-1 md:grid-cols-2 gap-5">
+        <div className="p-10 bg-red-900"></div>
+        <div className="p-10 bg-blue-900"></div>
+        <div className="p-10 bg-gray-900"></div>
+        <div className="p-10 bg-yellow-900"></div>
+      </div>
+    </div>
+  );
 };
 
 export default Overview;
