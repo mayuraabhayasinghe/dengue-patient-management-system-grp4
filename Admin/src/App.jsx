257d0448a17c66019cb4b2ed743581c198dd4d04
Thushan2002 <selvakumarthushanthan4@gmail.com>
2025-05-12 16:20:10 +0530

    feat: admin home page added

--- Admin/src/App.jsx
+++ Admin/src/App.jsx
@@ -3,12 +3,15 @@ import { Route, Routes } from "react-router-dom";
 import Dashboard from "./Pages/Dashboard";
 import AdminNavbar from "./Components/Navbar";
 import AdminFeedbacks from "./Pages/AdminFeedbacks";
+import AdminHome from "./Pages/AdminHome";
 
 const App = () => {
   return (
     <>
       <AdminNavbar />
       <Routes>
+        <Route path="/" element={<AdminHome />}></Route>
+        <Route path="/admin" element={<AdminHome />}></Route>
         <Route path="/admin/dashboard" element={<Dashboard />}></Route>
         <Route path="/admin/feedback" element={<AdminFeedbacks />}></Route>
       </Routes>
