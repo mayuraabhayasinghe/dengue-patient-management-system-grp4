275153b6d2ede8c979ae5bef77f70ec9496633e0
Mevini Silva <silvamevini19@gmail.com>
2025-05-24 20:53:17 +0530

    resolved conflicts in "Caretaker input backend #90"

--- Server/server.js
+++ Server/server.js
@@ -26,6 +26,12 @@ app.use("/api/fluid", require("./routes/fluid"));
 
 // app.use("/api/patients", require("./routes/patientRoutes"));
 
+// Routes:
+app.use("/api/feedback", require("./routes/feedbackRoutes"));
+app.use("/api/patients", require("./routes/patientListRoutes")); // Updated this line
+app.use("/api/auth", require("./routes/authRoutes"));
+app.use("/api/staff", require("./routes/staffRoutes"));
+
 const PORT = process.env.PORT || 5000;
 app.listen(PORT, () => {
   console.log(`🚀 Server running on port ${PORT}`);
