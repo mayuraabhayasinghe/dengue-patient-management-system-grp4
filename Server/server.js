3dda6865cf68325f959615b7928b9f5e9fc8fb0e
Nadun Thilina <152951157+nadunthilina@users.noreply.github.com>
2025-05-30 15:14:08 +0530

    Update server.js

--- Server/server.js
+++ Server/server.js
@@ -3,18 +3,9 @@ const cors = require("cors");
 const dotenv = require("dotenv");
 const bodyParser = require("body-parser");
 const connectDB = require("./config/db");
-const path = require("path");
-const fs = require("fs");
 
 dotenv.config();
 connectDB();
-
-// Create uploads directory if it doesn't exist
-const uploadDir = path.join(__dirname, 'uploads');
-if (!fs.existsSync(uploadDir)) {
-  fs.mkdirSync(uploadDir, { recursive: true });
-  console.log('Uploads directory created');
-}
 
 const app = express();
 
@@ -23,9 +14,6 @@ app.use(cors());
 app.use(express.json());
 app.use(bodyParser.urlencoded({ extended: true }));
 
-// Serve static files from uploads directory
-app.use('/uploads', express.static('uploads'));
-
 // Test route
 app.get("/", (req, res) => {
   res.send("DengueGuard API is running...");
@@ -33,11 +21,10 @@ app.get("/", (req, res) => {
 
 // Routes:
 app.use("/api/feedback", require("./routes/feedbackRoutes"));
-app.use("/api/patients", require("./routes/patientsRoutes")); // Using our consolidated routes
+app.use("/api/patients", require("./routes/patientListRoutes")); // Updated this line
 app.use("/api/auth", require("./routes/authRoutes"));
 app.use("/api/staff", require("./routes/staffRoutes"));
-app.use("/api/profile", require("./routes/profileRoutes"));
-app.use("/api/vitals", require("./routes/patientVitalsRoutes"));
+app.use("/api/fluid", require("./routes/fluid"));
 
 const PORT = process.env.PORT || 5000;
 app.listen(PORT, () => {
