7896f0a2002831b1a0ec8a3f0c7fa277a3336f74
mayuraabhayasinghe <abhayasinghemayura@gmail.com>
2025-05-31 19:06:04 +0530

    patient listing feature is created

--- Server/routes/patientsRoutes.js
+++ Server/routes/patientsRoutes.js
@@ -1,7 +1,11 @@
 const express = require("express");
 const router = express.Router();
-const { addPatient } = require("../controllers/patientController");
+const {
+  addPatient,
+  getAllPatients,
+} = require("../controllers/patientController");
 
 router.post("/register-patient", addPatient);
+router.get("/", getAllPatients);
 
 module.exports = router;
