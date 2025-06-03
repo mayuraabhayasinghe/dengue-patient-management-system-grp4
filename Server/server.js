e5f0b2c1dc54ee5640007150147869f05ddd1fb1
Narmada2001 <155349750+Narmada2001@users.noreply.github.com>
2025-06-03 14:25:05 +0530

    create addWard-backend

--- Server/server.js
+++ Server/server.js
@@ -25,6 +25,7 @@ app.use("/api/patients", require("./routes/patientsRoutes")); // Updated this li
 app.use("/api/auth", require("./routes/authRoutes"));
 app.use("/api/staff", require("./routes/staffRoutes"));
 app.use("/api/fluid", require("./routes/fluid"));
+app.use("/api/wards", require("./routes/wardRoutes"));
 
 const PORT = process.env.PORT || 5000;
 app.listen(PORT, () => {
