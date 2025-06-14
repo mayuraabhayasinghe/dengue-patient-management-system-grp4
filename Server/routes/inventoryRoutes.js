c3a7657ca22626d09bdb23626a053c5860b675e2
Mevini Silva <silvamevini19@gmail.com>
2025-06-14 11:33:07 +0530

    update controller

--- Server/routes/inventoryRoutes.js
+++ Server/routes/inventoryRoutes.js
@@ -1,7 +1,16 @@
 const express = require("express");
 const router = express.Router();
-const { addInventoryItem } = require("../controllers/inventoryController");
+const {
+  addInventoryItem,
+  getInventoryItems,
+  updateInventoryItem,
+  deleteInventoryItem,
+} = require("../controllers/inventoryController");
 
+// Remove the redundant "/inventory" prefix
 router.post("/", addInventoryItem);
+router.get("/", getInventoryItems);
+router.put("/:id", updateInventoryItem);
+router.delete("/:id", deleteInventoryItem);
 
 module.exports = router;
